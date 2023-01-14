a7047cafb227d10247f340fa1994cf55f1585196
Arthur Koba <caprimores@gmail.com>
2023-01-14 20:48:02 +0300

    update register function

--- src/endpoints/register.py
+++ src/endpoints/register.py
@@ -1,6 +1,10 @@
-from loader import app
-from models import RegisterModel
+from sqlalchemy import select, insert
+from sqlalchemy.exc import IntegrityError
 
+from loader import app
+from db.models import Users
+from db.debs import get_db
+from models import RegisterModel
 
 @app.get("/checkAvailabilityUsername/{username}")
 async def check_availability_username(username: str):
@@ -9,4 +13,13 @@ async def check_availability_username(username: str):
 
 @app.post("/register/")
 async def register_user(register_data: RegisterModel):
-    return {"is_availability": False}
+    data = register_data.dict()
+    data.update(password_hash=data.pop("password"))
+    query = insert(Users).values(**data)
+    db = get_db()
+    try:
+        await db.execute(query)
+        await db.commit()
+    except IntegrityError as e:
+        return {"registered": False, "error": str(e)}
+    return {"registered": True}
